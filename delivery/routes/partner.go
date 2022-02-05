a99370bb2608076dac42b16ccf308b6d3b54411e
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-05 15:12:31 +0700

    add jwt middleware to partner routes

--- delivery/routes/partner.go
+++ delivery/routes/partner.go
@@ -10,9 +10,9 @@ import (
 func RegisterPartnerPath(e *echo.Echo, partnerCtrl *partner.PartnerController) {
 
 	e.POST("/partners", partnerCtrl.ApplyPartner(), middleware.JWT([]byte(constants.JWT_SECRET_KEY)))
-	e.GET("/partners", partnerCtrl.GetAllPartner())
+	e.GET("/partners", partnerCtrl.GetAllPartner(), middleware.JWT([]byte(constants.JWT_SECRET_KEY)))
 	e.DELETE("/partners", partnerCtrl.DeletePartner(), middleware.JWT([]byte(constants.JWT_SECRET_KEY)))
-	e.PUT("/partners/:id/accept", partnerCtrl.AcceptPartner())
-	e.PUT("/partners/:id/reject", partnerCtrl.RejectPartner())
+	e.PUT("/partners/:id/accept", partnerCtrl.AcceptPartner(), middleware.JWT([]byte(constants.JWT_SECRET_KEY)))
+	e.PUT("/partners/:id/reject", partnerCtrl.RejectPartner(), middleware.JWT([]byte(constants.JWT_SECRET_KEY)))
 	// e.GET("/partners/:id", partnerCtrl.GetAllPartnerProduct())
 }
