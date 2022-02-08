a0f11e09485f851bc6727f08b90b68c3723d6c78
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-08 17:06:21 +0700

    fix seeder

--- seeder/partnerSeeder.go
+++ seeder/partnerSeeder.go
@@ -1,13 +1,27 @@
 package seeder
 
 import (
+	"github.com/furqonzt99/snackbox/helper"
 	"github.com/furqonzt99/snackbox/models"
 	"gorm.io/gorm"
 )
 
 func PartnerSeeder(db *gorm.DB)  {
+	password, _ := helper.Hashpwd("1234qwer")
+	user := models.User{
+		Name:         "User 1",
+		Email:        "user1@gmail.com",
+		Password:     password,
+		Address:      "Jl Matraman No 13",
+		City:         "Jakarta",
+		Balance:      0,
+		Role: "partner",
+	}
+
+	db.Create(&user)
+
 	partner1 := models.Partner{
-		UserID:        2,
+		UserID:        3,
 		BussinessName: "Melati Katering",
 		Description:   "Katering Murah Rasa Bintang 5",
 		Latitude:      -7.7343187,
