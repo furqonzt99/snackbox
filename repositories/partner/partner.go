6d14229f438017f74466cc16d980399d27ce7f85
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-14 14:41:29 +0700

    add calculate avg partner rating & partner ratings

--- repositories/partner/partner.go
+++ repositories/partner/partner.go
@@ -13,7 +13,6 @@ type PartnerInterface interface {
 	FindUserId(userId int) (models.Partner, error)
 	AcceptPartner(partner models.Partner) error
 	RejectPartner(partner models.Partner) error
-	GetAllPartnerProduct() ([]models.Partner, error)
 	UploadDocument(partnerID int, partner models.Partner) (models.Partner, error)
 }
 
@@ -111,20 +110,10 @@ func (p *PartnerRepository) RejectPartner(partner models.Partner) error {
 	return nil
 }
 
-func (p *PartnerRepository) GetAllPartnerProduct() ([]models.Partner, error) {
-	var partner []models.Partner
-
-	err := p.db.Preload("Products").Find(&partner).Error
-	if err != nil {
-		return nil, err
-	}
-	return partner, nil
-}
-
 func (p *PartnerRepository) GetPartner(partnerId int) (models.Partner, error) {
 
 	var partner models.Partner
-	err := p.db.Preload("Products").First(&partner, partnerId).Error
+	err := p.db.Preload("Ratings").Preload("Products").Preload("User").First(&partner, partnerId).Error
 	if err != nil {
 		return partner, err
 	}
