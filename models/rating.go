777e81a2b18f3f2670ceccfdeb84c19b072f3f10
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-14 14:25:11 +0700

    add get rating by trxID

--- models/rating.go
+++ models/rating.go
@@ -1,10 +1,12 @@
 package models
 
 type Rating struct {
+	TransactionID uint
 	PartnerID uint
 	UserID uint
 	Rating int
 	Comment string
+	Transaction Transaction
 	User User
 	Partner Partner
 } 
